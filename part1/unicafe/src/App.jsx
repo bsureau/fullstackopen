df797b3beaea63b50434b9ccd9f63b6eefe52ed0
Benjamin SUREAU <sureau.benjamin@gmail.com>
2024-09-14 11:40:39 +0200

    feat: complete exercises part1 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,27 @@ import './App.css'
 
 const Title = ({ title }) => <h2>{title}</h2>
 const Button = ({ eventHandler, text }) => <button onClick={eventHandler}>{text}</button>
-const Stat = ({ text, stat }) => <p>{text} {stat}</p>
+const dataFormat = {
+  number: '',
+  percent: '%'
+}
+const StatisticLine = ({ text, stat, format }) => <p>{text} {stat} {dataFormat[format]}</p>
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <>
+        <StatisticLine text='Good' stat={good} format='number' />
+        <StatisticLine text='Neutral' stat={neutral} format='number' />
+        <StatisticLine text='Bad' stat={bad} format='number' />
+        <StatisticLine text='All' stat={good + neutral + bad} format='number' />
+        <StatisticLine text='Average' stat={(good + bad * -1) / (good + neutral + bad)} format='number' />
+        <StatisticLine text='Positive' stat={good / (good + neutral + bad)} format='percent' />
+      </>
+    )
+  }
+}
 
 function App() {
   const [good, setGood] = useState(0)
@@ -17,7 +37,6 @@ function App() {
   const handleNeutralClick = () => setNeutral(neutral + 1)
   const handleBadClick = () => setBad(bad + 1)
 
-
   return (
     <>
       <Title title="give feedback" />
@@ -25,9 +44,7 @@ function App() {
       <Button eventHandler={handleNeutralClick} text="neutral" />
       <Button eventHandler={handleBadClick} text="bad" />
       <Title title="statistics" />
-      <Stat text='Good' stat={good} />
-      <Stat text='Neutral' stat={neutral} />
-      <Stat text='Bad' stat={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
