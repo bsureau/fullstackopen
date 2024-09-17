489e7a29566d0cae0c59945ecd5a2c79091bb266
Benjamin SUREAU <sureau.benjamin@gmail.com>
2024-09-17 17:00:54 +0200

    feat: complete exercises part2 phonebook step 11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
+import axios from 'axios'
 import Filter from './component/Filter'
 import PersonForm from './component/PersonForm'
 import Persons from './component/Persons'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filter, setFilter] = useState('')
@@ -34,6 +30,14 @@ const App = () => {
     else
       setPersons(persons.concat({ name: newName, phone: newPhone, id: persons.length + 1 }))
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
